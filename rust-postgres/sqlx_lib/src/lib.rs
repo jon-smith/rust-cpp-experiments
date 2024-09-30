40aaf5c5ca4c144b356d43cb86c8d8419bd7984d
jon-smith <26364802+jon-smith@users.noreply.github.com>
2024-09-30 19:19:43 +0100

    function to add row to db

--- rust-postgres/sqlx_lib/src/lib.rs
+++ rust-postgres/sqlx_lib/src/lib.rs
@@ -1,4 +1,6 @@
 use dotenvy::dotenv;
+use serde_json::json;
+use sqlx::postgres::PgPool;
 use sqlx::postgres::PgPoolOptions;
 use sqlx::types::chrono;
 use sqlx::types::Json;
@@ -56,6 +58,42 @@ pub async fn read_all_rows_async() -> Result<Vec<RowData>, sqlx::Error> {
         .await?;
 
     Ok(rows)
+}
+
+pub async fn clear_table_and_add_single_row_async() -> Result<i32, sqlx::Error> {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let pool = PgPool::connect(&database_url).await?;
+
+    sqlx::query!("TRUNCATE datatable").execute(&pool).await?;
+
+    let info = String::from("info");
+    let date_time = chrono::Utc::now().naive_utc();
+    let data = json!({
+        "name": "wiseguy",
+        "number": 43,
+        "serial-numbers": [
+            "x30495mmn",
+            "pp-jj6654"
+        ]
+    });
+
+    let rec = sqlx::query!(
+        r#"
+    INSERT INTO datatable ( info, data, time )
+    VALUES ( $1, $2, $3 )
+    RETURNING id
+            "#,
+        info,
+        Json(data) as _,
+        date_time,
+    )
+    .fetch_one(&pool)
+    .await?;
+
+    Ok(rec.id)
 }
 
 pub fn read_all_rows() -> Result<Vec<RowData>, sqlx::Error> {
